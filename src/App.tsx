ba81f551d94edcf3d1474d353f3667ef39172df6
Hyun_Jun <hyunjundev8904@gmail.com>
2026-05-24 12:31:22 +0900

    tailwind start

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import ButtonGroup from "./components/ButtonGroup.tsx";
+
 export default function App() {
-  const initialButtons = [
-    {id: 1, label: "Button 1", icon: "🔥", isDisabled: false},
-    {id: 2, label: "Button 2", icon: "💧", isDisabled: false},
-    {id: 3, label: "Button 3", icon: "🌱", isDisabled: true},
-    {id: 4, label: "Button 4", icon: "⚡", isDisabled: false},
-  ];
+
 
   return (
     <>
-      <ButtonGroup initialButtons={initialButtons}
-      ></ButtonGroup>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   );
 };
--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -1,60 +1,58 @@
-import styles from '../ButtonGroup.module.css';
+import style from '../ButtonGroup.module.css'
 import classNames from 'classnames/bind';
 import {useState} from "react";
 
-export default function ButtonGroup({initialButtons}: {
-  initialButtons: {
-    id: number;
-    label: string;
-    icon: string;
-    isDisabled: boolean;
-  }[];
-}) {
-  const cx = classNames.bind(styles);
 
-  const initActiveState: { [key: string]: boolean } = {};
-  initialButtons.forEach((btn) => {
-    initActiveState[btn.id] = false;
+export default function ButtonGroup({initialButtons}: {
+  initialButtons: { id: number; label: string; icon: string; isDisabled: boolean; }[];
+}) {
+
+  const cx = classNames.bind(style);
+
+  const activeState: { [key: string]: boolean } = {}
+  initialButtons.forEach((item) => {
+    activeState[item.id] = false;
   })
 
-  const [isActive, setIsActive] = useState(initActiveState);
-  const handleToggle = (id: number) => {
-    console.log(id)
-    if (id === 3) return;
-    setIsActive((isActive) => ({
-      ...isActive,
-      [id]: !isActive[id]
+  const [initActive, setInitActive] = useState(activeState);
+
+  const handleActiveToggle = (id: number) => {
+    if(id === 3) return
+    setInitActive((initActive) => ({
+      ...initActive,
+      [id]: !initActive[id]
     }))
   }
 
-  const handleResetBtn = () => {
-    const resetActiveState: { [key: string]: boolean } = {};
-    initialButtons.forEach((btn) => {
-      resetActiveState[btn.id] = false;
-    });
-    setIsActive(resetActiveState);
+  const count = Object.values(initActive).filter(Boolean).length;
+  console.log(count);
+
+  const handleReset = () => {
+    const resetState: { [key: string]: boolean } = {}
+    initialButtons.forEach((item) => {
+      resetState[item.id] = false;
+    })
+    setInitActive(resetState);
   }
 
-  const activeCnt = Object.values(isActive).filter(Boolean).length;
-  console.log("activeCnt", activeCnt);
 
   return (
     <>
-      <h1>Active Count : {activeCnt}</h1>
+      <h1>Active Count: {count}</h1>
       {
         initialButtons.map((btn) =>
-          <button key={btn.id}
-                  className={cx("button",
-                    {
-                      "disabled": btn.isDisabled,
-                      "active": isActive[btn.id],
-                      "highlight": btn.id === 2
-                    },
-                  )}
-                  onClick={() => handleToggle(btn.id)}
-          >{btn.label} {btn.icon}</button>)
+          <button key={btn.id} className={cx(
+            "button",
+            {
+              "disabled": btn.isDisabled,
+              "highlight": btn.id === 2,
+              "active": initActive[btn.id],
+            }
+          )}
+            onClick={() => handleActiveToggle(btn.id)}
+          >{btn.icon} {btn.label}</button>)
       }
-      <button className={cx("button")} onClick={handleResetBtn}>reset</button>
+      <button className={cx("button")} onClick={handleReset}>reset</button>
     </>
   );
 };
