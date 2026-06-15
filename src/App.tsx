6aa2ba0421ed5af291e3f3760b2d69ff1e3a11cf
Hyun_Jun <hyunjundev8904@gmail.com>
2026-06-15 09:53:32 +0900

    UserProfile 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import TodoList from "./components/conponent/TodoApp.tsx";
-import CountApp from "./components/conponent/sideEffect/CountApp.tsx";
-import {useCallback, useState} from "react";
-import UseMemo from "./components/conponent/etc/UseMemo.tsx";
-import TestApp from "./components/conponent/etc/TestApp.tsx";
-import App13 from "./components/conponent/secion13/App13.tsx";
-
+// import TodoList from "./components/conponent/TodoApp.tsx";
+// import CountApp from "./components/conponent/sideEffect/CountApp.tsx";
+// import {useCallback, useState} from "react";
+// import UseMemo from "./components/conponent/etc/UseMemo.tsx";
+// import TestApp from "./components/conponent/etc/TestApp.tsx";
+// import App13 from "./components/conponent/secion13/App13.tsx";
+import AppUserProfile from "./components/conponent/secion13/AppUserProfile.tsx";
 
 export default function App() {
 
   return (
     <>
-      <App13></App13>
+      <AppUserProfile></AppUserProfile>
+      {/*<App13></App13>*/}
       {/*<TestApp />*/}
       {/*<UseMemo/>*/}
       {/*<TodoList></TodoList>*/}
