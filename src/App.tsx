8d533dd761fc0558ee1682c0ddf2f8b38f993f68
Hyun_Jun <hyunjundev8904@gmail.com>
2026-06-22 10:00:18 +0900

    todoList contextAPI 입히기 전 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-// import TodoList from "./components/conponent/TodoApp.tsx";
+import TodoList from "./components/conponent/TodoApp.tsx";
 // import CountApp from "./components/conponent/sideEffect/CountApp.tsx";
 // import {useCallback, useState} from "react";
 // import UseMemo from "./components/conponent/etc/UseMemo.tsx";
 // import TestApp from "./components/conponent/etc/TestApp.tsx";
 // import App13 from "./components/conponent/secion13/App13.tsx";
-import AppUserProfile from "./components/conponent/secion13/AppUserProfile.tsx";
-import SettingProvider from "./context/userProfileSetting/SettingProvider.tsx";
+// import AppUserProfile from "./components/conponent/secion13/AppUserProfile.tsx";
+// import SettingProvider from "./context/userProfileSetting/SettingProvider.tsx";
 
 export default function App() {
 
   return (
     <>
-      <SettingProvider>
-        <AppUserProfile/>
-      </SettingProvider>
-      {/*<App13></App13>*/}
+      {/*<SettingProvider>*/}
+      {/*  <AppUserProfile/>*/}
+      {/*</SettingProvider>*/}
+
+      {/*<App13 />*/}
       {/*<TestApp />*/}
       {/*<UseMemo/>*/}
-      {/*<TodoList></TodoList>*/}
-      {/*<CountApp></CountApp>*/}
+      <TodoList/ >
+      {/*<CountApp/>*/}
     </>
   );
 };
